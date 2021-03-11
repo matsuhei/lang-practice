ec403bb31b898acedfbddddd48c188cf9298248f
matsuhei <matsumoto_ryohei@cam-inc.co.jp>
2021-03-11 13:17:58 +0900

    update readme

--- java/Lottery/src/Singleton/Test.java
+++ java/Lottery/src/Singleton/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         Context context = new Context();
         System.out.println(context.getClassA().getClassName() == "ClassA");
+        System.out.println(context.getClassB().getClassName() == "ClassB");
     }
 }
